c7cdd288f2f94367513f5fd3e4f92ae1ccee8b6f
Ajithpradeep01 <ajithachu2k18@gmail.com>
2023-12-29 09:18:03 +0530

    complete about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
-      About
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>WELCOME TO HUB.COM</h1>
+      <p className='mb-8 text-slate-700' >At Hub.com, we believe in connecting dreams to addresses, transforming the way you experience real estate. As a cutting-edge estate marketplace, fostering a community where property meets possibility.</p>
+      <p className='mb-8 text-slate-700'> Explore Boundless Opportunities: Dive into a diverse array of real estate options, from cozy apartments to sprawling estates, meticulously curated to cater to every lifestyle and preference.</p>
+      <p className='mb-8 text-slate-700'>Seamless Connectivity: Hub.com is more than a marketplace; it's a hub for connectivity. Connect with like-minded individuals, real estate experts, and potential buyers or sellers, creating a network that transcends property boundaries.</p>
+      <p className='mb-8 text-slate-700'>Empowering Sellers: List your property with confidence on Hub.com. Leverage our robust tools and resources to showcase your property in its best light, reaching a vast audience of potential buyers.</p>
+      <p className='mb-6 text-slate-700'> Your Journey, Your Hub: Whether you're a first-time homebuyer, an experienced investor, or a seller looking to make a change, Hub.com is your dedicated space to embark on a real estate journey that aligns with your unique aspirations.</p>
+
+      <p className='text-xs font-bold text-slate-500  text-center'>Join us at Hub.com and let the journey to your dream property begin!</p>
     </div>
   )
 }
