51c36335b8f5d35b965a8e154d6dfa32d57188c4
Ajithpradeep01 <ajithachu2k18@gmail.com>
2023-12-29 08:57:43 +0530

    complete home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,8 +29,8 @@ function Header() {
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
         <h1 className='font-bold text-sm"text-xl flex flex-wrap'>
-            <span className='text-slate-500'>SREERAM</span>
-            <span className='text-slate-700'>MUSICALS</span>
+            <span className='text-slate-500'>HUB</span>
+            <span className='text-slate-700'>.COM</span>
 
         </h1>
         </Link>
